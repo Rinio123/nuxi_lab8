4451bfe320b945e80c0bf692f6f59a317c9be696
Rinio123 <untiminhoo86@gmail.com>
2024-12-05 08:08:01 +0700

    12-5-nuxi

---
+++ app.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import nuxtConfig from './nuxt.config';
+</script>
+
+<!-- <template>
+  <div>
+    <NuxtRouteAnnouncer />
+    <NuxtWelcome />
+  </div>
+</template> -->
+
+<template>
+  <NuxtPage/>
+</template>
